0191fb9cf9df64d70e25e72820920d5a914a557d
k-start <startkie@gmail.com>
2020-12-23 17:08:04 +1100

    Extremely bad and basic sleep function

--- kernel/PIT.cpp
+++ kernel/PIT.cpp
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <kernel/ports.h>
+#include <kernel/kstdio.h>
 
 PIT *PIT::the;
 
@@ -22,10 +23,25 @@ void PIT::handleIRQ() {
     if(ticks%100 == 0) {
         seconds++;
     }
+
+    if(sleeping) {
+        sleepMS += 10;
+    }
 }
 
 String PIT::getUptimeStr() {
     String string;
     string += seconds;
     return string;
+}
+
+// FIX-ME VERY BAD IMPLEMENTATION
+void PIT::sleep(int ms) {
+    sleepMS = 0;
+    sleeping = true;
+
+    while(sleepMS < ms) {
+        sout("");
+    }
+    sout("slept\n");
 }
