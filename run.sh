f1d65875b9b34410981cea4a53dee549f9dce30e
k-start <startkie@gmail.com>
2021-01-08 22:27:03 +1100

    run command points to a batch file to run on windows for WSL

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@ set -e
 . ./build.sh
 
 if grep -q Microsoft /proc/version; then
-  qemu-system-$(./target-triplet-to-arch.sh $HOST).exe -kernel kernel/untitledos.kernel -serial stdio -no-reboot -monitor stdio -no-shutdown
+  cmd.exe /c 'E:\wsl\installed\ubuntu-20.04\rootfs\home\kizzington\UntitledOS\windows.bat'
+  # qemu-system-$(./target-triplet-to-arch.sh $HOST).exe -kernel kernel/untitledos.kernel -serial stdio -no-reboot -monitor stdio -no-shutdown
   # qemu-system-$(./target-triplet-to-arch.sh $HOST).exe -kernel kernel/untitledos.kernel -serial stdio -no-reboot
 else
   qemu-system-$(./target-triplet-to-arch.sh $HOST) -kernel kernel/untitledos.kernel -serial stdio -no-reboot
